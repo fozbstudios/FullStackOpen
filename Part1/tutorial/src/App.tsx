32282c9f71e7a1137d200d9d3d140983641d1161
Nick K <fozbstudios@gmail.com>
2024-05-10 16:18:37 -0500

    followed tutorial but used typescipt instead

--- Part1/tutorial/src/App.tsx
+++ Part1/tutorial/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+type Person = {
+    name: string
+    age: number
+}
+function Hello(props: Person) {
+    return (
+        <div>
+            <p>Hello {props.name} you are {props.age}</p>
+        </div>
+    )
+}
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Greetings</h1>
+            <Hello name='Joe' age={43} />
+            <Hello name='Tom' age={53} />
+            <Hello name='Nick' age={68} />
+            <Footer />
+        </>
+    )
+}
+function Footer() {
+    return (
+        <div>
+            greeting app created by <a href='https://github.com/fozbstudios'>fozbstudios</a>
+        </div>
+    )
 }
 
 export default App
