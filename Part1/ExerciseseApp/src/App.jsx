3fa3ed53d8890fc5fa0f7fc90b7b9af801ac40fd
Nick K <fozbstudios@gmail.com>
2024-05-11 06:24:37 -0500

    exercise1.5

--- Part1/ExerciseseApp/src/App.jsx
+++ Part1/ExerciseseApp/src/App.jsx
@@ -16,7 +16,7 @@ const Content = (props) => {
             <Part info={props.parts[1]} />
             <Part info={props.parts[2]} />
         </>
-        )
+    )
 
 }
 const Part = (props) => {
@@ -24,27 +24,29 @@ const Part = (props) => {
 
 }
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
     return (
 
         <div>
-            <Header name={course} />
-            <Content parts={parts} />
-            <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
         </div>
     )
 }
